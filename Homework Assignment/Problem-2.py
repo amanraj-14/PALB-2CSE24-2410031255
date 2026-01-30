ca112bbb33a7da30271afadc92d9683e720af90c
amanraj-14 <amanr835222@gmail.com>
2026-01-30 14:07:36 +0530

    Add files via upload

---
+++ Homework Assignment/Problem-2.py
@@ -0,0 +1,12 @@
+arr = [1, 4, 3, 5, 8, 6]
+
+min_val = arr[0]
+max_val = arr[0]
+
+for i in arr:
+    if i < min_val:
+        min_val = i
+    if i > max_val:
+        max_val = i
+
+print(min_val, max_val)
