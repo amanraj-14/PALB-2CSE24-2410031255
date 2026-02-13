2d7941417e17f392adc239baf27d9c76d51c129e
amanraj-14 <amanr835222@gmail.com>
2026-02-13 09:53:55 +0530

    Update Problem-20.py

--- Homework Assignment/Problem-20.py
+++ Homework Assignment/Problem-20.py
@@ -1,9 +1,17 @@
-n = 10
+def isSubset(a, b):
+    freq = {}
 
-fact = 1
-for i in range(1, n + 1):
-    fact *= i
+    for num in a:
+        freq[num] = freq.get(num, 0) + 1
 
-result = list(map(int, str(fact)))
+    for num in b:
+        if num not in freq or freq[num] == 0:
+            return False
+        freq[num] -= 1
 
-print(result)
+    return True
+
+
+print(isSubset([11, 7, 1, 13, 21, 3, 7, 3], [11, 3, 7, 1, 7]))
+print(isSubset([1, 2, 3, 4, 4, 5, 6], [1, 2, 4]))
+print(isSubset([10, 5, 2, 23, 19], [19, 5, 3]))
