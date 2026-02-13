6be1f3a7302be7648205f80fa12675b2b851954f
amanraj-14 <amanr835222@gmail.com>
2026-02-13 10:20:31 +0530

    Update Problem-13.py

--- Homework Assignment/Problem-13.py
+++ Homework Assignment/Problem-13.py
@@ -1,27 +1,34 @@
-arr = [2, 3, 1, 1, 4]
-
-n = len(arr)
-
-if n <= 1:
-    print(0)
-elif arr[0] == 0:
-    print(-1)
-else:
+def minJumps(arr):
+    n = len(arr)
+    
+    if n <= 1:
+        return 0
+    
+    if arr[0] == 0:
+        return -1
+    
     jumps = 1
     maxReach = arr[0]
     steps = arr[0]
     
     for i in range(1, n):
         if i == n - 1:
-            print(jumps)
-            break
+            return jumps
         
         maxReach = max(maxReach, i + arr[i])
         steps -= 1
         
         if steps == 0:
             jumps += 1
+            
             if i >= maxReach:
-                print(-1)
-                break
+                return -1
+            
             steps = maxReach - i
+    
+    return -1
+
+
+print(minJumps([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]))
+print(minJumps([1, 4, 3, 2, 6, 7]))
+print(minJumps([0, 10, 20]))
